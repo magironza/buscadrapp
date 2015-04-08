9de97b04369681259ca82fc2e8769c558cbe64b3
Alejandro Gironza <magironza@gmail.com>
2015-04-07 22:03:30 -0500

    segundo repositorio, funciona el long list y ya casi esta toda la estructura de la aplicacion, falta el root, usuario, comentarios ...

--- Buscadr/Buscadr/Busqueda.xaml.cs
+++ Buscadr/Buscadr/Busqueda.xaml.cs
@@ -7,14 +7,32 @@ using System.Windows.Controls;
 using System.Windows.Navigation;
 using Microsoft.Phone.Controls;
 using Microsoft.Phone.Shell;
+using Buscadr.Resources;
+using Buscadr.Net;
+using Buscadr.Models;
 
 namespace Buscadr
 {
-    public partial class Busqueda : PhoneApplicationPage
+    public partial class Busqueda : PhoneApplicationPage, Mongo<Comentario>.Imongo
     {
+        Mongo<Comentario> mongo;
+
         public Busqueda()
         {
+
             InitializeComponent();
+            mongo = new Mongo<Comentario>("ppZxpAloVZB2iYVatT1rEz_6_bXUf0RJ", "listadrpop" , "drpopayan");
+            mongo.findAllDocuments(this);
+        }
+
+        public void loadDocuments(List<Comentario> documents)
+        {
+            DataModel dataM = Application.Current.Resources["dataModel"] as DataModel;
+
+            for (int i = 0; i < documents.Count; i++)
+            {
+                dataM.Data.Add(documents.ElementAt(i));
+            }
         }
     }
 }
--- Buscadr/Buscadr/Net/mongo.cs
+++ Buscadr/Buscadr/Net/mongo.cs
@@ -9,46 +9,69 @@ using Windows.Web.Http.Headers;
 
 namespace Buscadr.Net
 {
-    
 
-   public class Mongo<T>
+
+    public class Mongo<T>
     {
-       const String URL_BASE = "https://api.mongolab.com/api/1/databases/";
-
-       HttpClient client;
-
-       String url;
-       String apiKey;
-
-       public Mongo(String apiKey, String dbName, String collectionName) {
-           client = new HttpClient();
-
-           this.apiKey= "apiKey="+apiKey;
-           url = URL_BASE + dbName + "/collections/" + collectionName + "?" + this.apiKey;
-       
-       }
-
-       public async void insertDocument(T document)
-       {
-           JsonSerializerSettings property = new JsonSerializerSettings();
-           property.NullValueHandling = NullValueHandling.Ignore;
-
-           String json = JsonConvert.SerializeObject(document,Formatting.None, property);
-
-           HttpStringContent content =new HttpStringContent(json);
-
-           HttpMediaTypeHeaderValue contentType = new HttpMediaTypeHeaderValue("application/json");
+        public interface Imongo
+        {
+            void loadDocuments(List<T> documents);
+        }
 
 
-           content.Headers.ContentType = contentType;
+        const String URL_BASE = "https://api.mongolab.com/api/1/databases/";
 
-           await client.PostAsync(new Uri(url), content);
+        HttpClient client;
+
+        String url;
+        String apiKey;
+        Imongo iMongo;
+
+        public Mongo(String apiKey, String dbName, String collectionName)
+        {
+            client = new HttpClient();
+
+            this.apiKey = "apiKey=" + apiKey;
+            url = URL_BASE + dbName + "/collections/" + collectionName + "?" + this.apiKey;
+
+        }
+
+        public async void insertDocument(T document)
+        {
+            JsonSerializerSettings property = new JsonSerializerSettings();
+            property.NullValueHandling = NullValueHandling.Ignore;
+
+            String json = JsonConvert.SerializeObject(document, Formatting.None, property);
+
+            HttpStringContent content = new HttpStringContent(json);
+
+            HttpMediaTypeHeaderValue contentType = new HttpMediaTypeHeaderValue("application/json");
+
+
+            content.Headers.ContentType = contentType;
+
+            await client.PostAsync(new Uri(url), content);
+        }
 
 
 
+        public async void findAllDocuments(Imongo iMongo)
+        {
+            HttpResponseMessage msg = await client.GetAsync(new Uri (url));
+            String jsonArray = msg.Content.ToString();
 
-       }
+            List<T> data = JsonConvert.DeserializeObject<List<T>>(jsonArray);
+            iMongo.loadDocuments(data);
 
-    
+
+        }
+
     }
 }
+
+
+
+
+
+
+
